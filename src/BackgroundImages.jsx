0c9dd84f301ad2b1693d608bb4c86f199e74ab53
Manidrag <100986711+Manidrag@users.noreply.github.com>
2025-02-22 17:48:55 +0530

    Update BackgroundImages.jsx

--- src/BackgroundImages.jsx
+++ src/BackgroundImages.jsx
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
-import image2 from "/src/assets/image2.jpg?url";
-import image3 from "/src/assets/image3.jpg?url"; // Corrected typo
-import image4 from "/src/assets/image4.jpg?url";
-import image1 from "/src/assets/image1.jpg?url";
-import image5 from "/src/assets/image5.jpg?url";
-import image6 from "/src/assets/image6.jpg?url";
-import image7 from "/src/assets/image7.jpg?url";
+import image2 from "src/assets/image2.jpg?url";
+import image3 from "src/assets/image3.jpg?url"; // Corrected typo
+import image4 from "src/assets/image4.jpg?url";
+import image1 from "src/assets/image1.jpg?url";
+import image5 from "src/assets/image5.jpg?url";
+import image6 from "src/assets/image6.jpg?url";
+import image7 from "src/assets/image7.jpg?url";
 
 export function BackgroundImg({ interval = 8000, children }) {
   const images = [image1, image2, image3, image4, image5, image6, image7];
